879a77efe22eef11bbaf8d36c7678bb9ad2c2f7f
Raul Vargas <rvargas38@ucmerced.edu>
2025-02-26 17:18:13 -0800

    added rays

--- Raytrace.cc
+++ Raytrace.cc
@@ -1,5 +1,8 @@
 ﻿// CMakeProject1.cpp : Defines the entry point for the application.
 //
+#include "color.h"
+#include "vec3.h"
+
 
 #include "Raytrace.h"
 
@@ -16,15 +19,10 @@ int main()
 	for (int j = 0; j < image_height; j++) {
 		std::clog << "r\Scanlines remaining: " << (image_height - j) << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++) {
-			auto r = double(i) / (image_height - 1);
-			auto g = double(j) / (image_width - 1);
-			auto b = 0;
-
-			int ir = int(255.999 * r);
-			int ig = int(255.999 * g);
-			int ib = int(255.999 * b);
-
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+			write_color(std::cout, pixel_color);
 		}
 	}
+
+	std::clog << "\rDone.                 \n";
 }
