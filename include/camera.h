51dcd7cf4e9ad4ec83fcc65e5e460ec7c01d46ca
Raul Vargas <rvargas38@ucmerced.edu>
2025-03-05 01:27:36 -0800

    added refraction

--- include/camera.h
+++ include/camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 
 class camera {
@@ -10,6 +11,11 @@ public:
 	int image_width				= 100;
 	int samples_per_pixels		= 10;
 	int max_depth				= 10;
+
+	double vfox = 90;
+	point3 lookfrom = point3(0, 0, 0);
+	point3 lookat = point3(0, 0, -1);
+	vec3 vup = vec3(0, 1, 0);
 
 	void render(const hittable& world) {
 		initialize();
@@ -38,6 +44,7 @@ private:
 	point3 pixel00_loc;
 	vec3 pixel_delta_u;
 	vec3 pixel_delta_v;
+	vec3 u, v, w;
 
 	void initialize() {
 		image_height = int(image_width / aspect_ratio);
@@ -45,24 +52,29 @@ private:
 
 		pixel_samples_scale = 1.0 / samples_per_pixels;
 
-		center = point3(0, 0, 0);
+		center = lookfrom;
 
 		// Determine viewport dimensions.
-		auto focal_length = 1.0;
-		auto viewport_height = 2.0;
+		auto focal_length = (lookfrom - lookat).length();
+		auto theta = degrees_to_radians(vfox);
+		auto h = std::tan(theta / 2);
+		auto viewport_height = 2 * h * focal_length;
 		auto viewport_width = viewport_height * (double(image_width) / image_height);
 
+		w = unit_vector(lookfrom - lookat);
+		u = unit_vector(cross(vup, w));
+		v = cross(w, u);
+
 		// Calculate the vectors across the horizontal and down the vertical viewport edges.
-		auto viewport_u = vec3(viewport_width, 0, 0);
-		auto viewport_v = vec3(0, -viewport_height, 0);
+		auto viewport_u = viewport_width * u;
+		auto viewport_v = viewport_height * -v;
 
 		// Calculate the horizontal and vertical delta vectors from pixel to pixel.
 		pixel_delta_u = viewport_u / image_width;
 		pixel_delta_v = viewport_v / image_height;
 
 		// Calculate the location of the upper left pixel.
-		auto viewport_upper_left =
-			center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+		auto viewport_upper_left = center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
 		pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 	}
 
@@ -91,8 +103,11 @@ private:
 		hit_record rec;
 
 		if (world.hit(r, interval(0.001, infinity), rec)) {
-			vec3 direction = random_on_hemisphere(rec.normal);
-			return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
+			ray scattered;
+			color attenuation;
+			if (rec.mat->scatter(r, rec, attenuation, scattered))
+				return attenuation * ray_color(scattered, depth - 1, world);
+			return color(0, 0, 0);
 
 		}
 
